ae5d19f14035b1df04afb45373e92422fcba64ab
ALFAREZ SYAHPUTRA KURI <kurialfarez@gmail.com>
2023-02-07 20:24:02 +0700

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,10 +14,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // buat variable untuk menangkap input horizontal dan vertical
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        //alternatif movement
+
+        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        //transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate (direction * _speed * Time.deltaTime);
+
+        //boundaries 
+        // kalau posisi lebih dari 0 player tidak boleh lebih dari 0
+        // kalau posisi lebih dari -3.8 player tidak boleh lebih dari -3.8
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
     }
 }

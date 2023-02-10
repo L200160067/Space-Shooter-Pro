1ca916805760d7054dc0ac56d71ea3f59673956a
ALFAREZ SYAHPUTRA KURI <kurialfarez@gmail.com>
2023-02-10 21:15:57 +0700

    new

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y <= -5f)
+        {
+            transform.position = new Vector3(Random.Range(-8.0f, 8.0f), 7f, 0);
+
+            //atau 
+            //float randomX = Random.Range(-8.0f, 8.0f);
+            //transform.position = new Vector3(randomX, 7f, 0);
+        }
+    }
+}
